6e6016d07cb6b5afcbc4a9a64a01f819913aa23a
admin <admin@admins-air>
2023-08-02 00:41:20 -0400

    2nd commit

--- Framework/src/test/java/com/learningAutomation/pages/LoginPage.java
+++ Framework/src/test/java/com/learningAutomation/pages/LoginPage.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.support.FindBy;
 
 public class LoginPage {
 	
+	
 	WebDriver driver;
 	
 	public LoginPage(WebDriver ldriver) {
@@ -24,7 +25,7 @@ public class LoginPage {
 	
 	public void clickLogin() throws InterruptedException {
 		login.click();
-		Thread.sleep(5000);
+		Thread.sleep(3000);
 	}
 	public void loginToCRM(String username,String password) {
 		uname.sendKeys(username);
